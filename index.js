9634050b003fc9245650a155eba9c6d7a6272c83
hargunsawhney <41484409+hargunsawhney@users.noreply.github.com>
2020-03-24 18:40:29 -0400

    wait command

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {Builder, By, Key, util} = require("selenium-webdriver");
 async function run(){
     let driver = await new Builder().forBrowser("chrome").build();
+    var wait = Math.floor(Math.random() * 1000);
+    console.log("Waiting " + wait + " seconds");
+    await new Promise(resolve => setTimeout(resolve, ((wait*1000) + 900)));
     await driver.get("https://docs.google.com/forms/d/e/1FAIpQLScei7p8nnbC-nOtgLJCSSO6XxU8vVmPfG7dCG5fD4VxIQLohQ/viewform");
     await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("hargun.sawhney2022@fairlawnsds.org");
     var month = new Date().getMonth() + 1;
@@ -16,7 +19,6 @@ async function run(){
     await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[7]/div/div[2]/div[2]/div[5]/span")).click();
     await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[1]/div/div/span/span")).click();
     await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/label/div/div[1]")).click();
-    console.log("done");
+    console.log("Attendance for " + month + "-" + day + "-" + year + " submitted. Check back later.");
 }
-
 run();
