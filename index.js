a05377b4744931f306c6a3b6dd3cc893abd4c19f
hargunsawhney <41484409+hargunsawhney@users.noreply.github.com>
2020-04-21 17:02:14 -0400

    new changes detected and added

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ async function run(){
     if(d.getDay() != 6 && d.getDay() != 7){
         await driver.get("https://docs.google.com/forms/d/e/1FAIpQLScei7p8nnbC-nOtgLJCSSO6XxU8vVmPfG7dCG5fD4VxIQLohQ/viewform");
         await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/input")).sendKeys("hargun.sawhney2022@fairlawnsds.org");
+        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[2]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("18711");
+        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Sawhney");
+        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[4]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Hargun");
+        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[5]/div/div[2]/div/span/div/div[3]/label/div/div[1]/div/div[3]/div")).click();
         await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[1]/div/div/span/span")).click();
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("18711");
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[4]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Sawhney");
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[5]/div/div[2]/div/div[1]/div/div[1]/input")).sendKeys("Hargun");
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[6]/div/div[2]/div/span/div/div[3]/label/div/div[1]/div/div[3]/div")).click();
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[1]/div/div[2]/span/span")).click();
         await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[2]/div[3]/div/div[2]/div[2]/div/label/div/div[1]/div[2]")).click();
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[1]/div/div[2]/span/span")).click();
-        await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[3]/div/div[2]/span/span")).click();
+        console.log("process works");
+        //await driver.findElement(By.xpath("//*[@id=\"mG61Hd\"]/div/div/div[3]/div[3]/div/div[2]/span/span")).click();
     }
 }
 run();
